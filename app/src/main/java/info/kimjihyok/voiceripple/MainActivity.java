c6b50988167281b1899c3775984dfe38f471a9ac
Jimmy Kim <jkimab@connect.ust.hk>
2017-08-26 21:09:50 +0900

    Added gif image

--- app/src/main/java/info/kimjihyok/voiceripple/MainActivity.java
+++ app/src/main/java/info/kimjihyok/voiceripple/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
     voiceRipple.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
     voiceRipple.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
-    // inner icon
+    // set inner icon
     voiceRipple.setRecordDrawable(ContextCompat.getDrawable(this, R.drawable.record), ContextCompat.getDrawable(this, R.drawable.recording));
     voiceRipple.setIconSize(30);
 
